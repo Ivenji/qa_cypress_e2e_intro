6c326ed7ba4afc19ecfcf72c68c2206a2f4f8d13
Ivenji <savgenkoe@gmail.com>
2025-05-15 20:17:33 -0400

    Solution5

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://conduit.mate.academy',
+    viewportHeight: 820,
+    viewportWidth: 500,
+    defaultCommandTimeout: 8000,
     setupNodeEvents(on, config) {
     }
   }
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,69 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+describe('user registration and Login Test', () => {
+  let email;
+  let password;
+  let username;
 
+  beforeEach(() => {
+    cy.visit('/');
+    cy.contains('Sign in').click();
+  });
+
+  it('should never log-in without advanced registration ', () => {
+    const randomNumber = Math.random().toString().slice(2);
+    username = `test_user_${randomNumber}`;
+    email = `${username}@gmail.com`;
+    password = 'Test123';
+
+    cy.get('h1').should('contain.text', 'Sign in');
+    cy.get('input[placeholder="Email"]').type(email);
+    cy.get('input[placeholder="Password"]').type('Test1234');
+    cy.get('.btn').click();
+    cy.get('.error-messages')
+      .should('have.text', 'email or password:is invalid');
+  });
+
+  it('should do log-in with the advanced registration ', () => {
+    const randomNumber = Math.random().toString().slice(2);
+    username = `test_user_${randomNumber}`;
+    email = `${username}@gmail.com`;
+    password = 'Test123';
+
+    Cypress.env('email', email);
+    Cypress.env('password', password);
+
+    cy.get(':nth-child(3) > .nav-link')
+      .should('contain.text', 'Sign up')
+      .click();
+
+    cy.get('h1')
+      .should('contain.text', 'Sign up');
+
+    cy.get(':nth-child(1) > .form-control')
+      .type(username);
+    cy.get(':nth-child(2) > .form-control')
+      .type(email);
+    cy.get(':nth-child(3) > .form-control')
+      .type(password);
+
+    cy.get('.btn').click();
+    cy.url().should('equal', Cypress.config().baseUrl + '/');
+
+    cy.get(':nth-child(3) > .nav-link')
+      .should('contain.text', 'Settings')
+      .click();
+
+    cy.get('button.btn-outline-danger').click();
+    cy.url()
+      .should('equal', Cypress.config().baseUrl + '/');
+
+    cy.get(':nth-child(2) > .nav-link').click();
+
+    cy.get(':nth-child(1) > .form-control').type(Cypress.env('email'));
+    cy.get(':nth-child(2) > .form-control').type(Cypress.env('password'));
+    cy.get('.btn').click();
+
+    cy.get('a.nav-link').contains('Global Feed');
   });
 });
